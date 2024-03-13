d733624379921818638388261fc2098b0477c3bb
15rudtp <thdrudtp15@naver.com>
2024-03-13 15:49:08 +0900

    오류 해결..

--- taskbox/.storybook/main.js
+++ taskbox/.storybook/main.js
@@ -16,3 +16,23 @@ const config = {
   },
 };
 export default config;
+
+
+
+// module.exports = {
+//   stories: ['../src/components/**/*.stories.js'],
+//    staticDirs: ['../public'],
+//    addons: [
+//      '@storybook/addon-links',
+//      '@storybook/addon-essentials',
+//      '@storybook/preset-create-react-app',
+//      '@storybook/addon-interactions',
+//    ],
+//    features: {
+//      postcss: false,
+//    },
+//    framework: '@storybook/react',
+//    core: {
+//      builder: 'webpack4',
+//    },
+//  };
